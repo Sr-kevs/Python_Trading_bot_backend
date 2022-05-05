eeda16811a9b4fa5a7fed937e4994be40e91346a
Sr-kevs <103867169+Sr-kevs@users.noreply.github.com>
2022-05-05 13:58:10 -0500

    Update apikeys.py

--- apikeys.py
+++ apikeys.py
@@ -1,2 +1,2 @@
-API_KEY = "zd78kY21dGVZZ3uh4XeKSBGaTX8NybnulU4WQ32ZLRW8nu08cU73oCbwGPoPYney"
-API_SECRET = "e6J6gkjTYx1ZMrzf6hCAciDqORAPfTwNMM97QZ8XUptuERB8xM8WrkNXyoYgVank"
+API_KEY = "yourbinanceapikey"
+API_SECRET = "yourbinanceapiscreteapi"
